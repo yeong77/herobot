7852c5935ea99b9a21a3f843af5af772be914535
yeong77 <124705989+yeong77@users.noreply.github.com>
2023-02-21 16:39:28 +0900

    Add files via upload

---
+++ src/models_top.py
@@ -0,0 +1,26 @@
+from torchvision import models as models
+import torch.nn as nn
+import os
+import torch, gc
+
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+gc.collect()
+torch.cuda.empty_cache()
+
+def model(pretrained, requires_grad):
+    model = models.resnet50(progress=True, pretrained=pretrained)
+    # to freeze the hidden layers
+    if requires_grad == False:
+        for param in model.parameters():
+            param.requires_grad = False
+    # to train the hidden layers
+    elif requires_grad == True:
+        for param in model.parameters():
+            param.requires_grad = True
+    # make the classification layer learnable
+    # we have 25 classes in total
+    model.fc = nn.Linear(2048, 18)
+    return model
+
